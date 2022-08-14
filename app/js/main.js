2ce912dc44cb4d52cad6c1f94cb294564aa86f22
David Halkin <halkin.david@gmail.com>
2022-08-14 16:06:55 +0300

    new

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,14 @@
 'use strict';
-$(document).ready(function () {
-
+// menu
+let menuBtn = document.querySelector('.menu-wrapper');
+let menu = document.querySelector('.nav_part');
+let body = document.querySelector('body');
+menuBtn.addEventListener('click', function () {
+	menuBtn.classList.toggle('animate');
+	menu.classList.toggle('opened');
+	body.classList.toggle('overflow');
 });
+
+// $(document).ready(function () {
+
+// });
