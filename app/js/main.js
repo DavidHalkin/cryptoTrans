d54e715988fc8399820a2acec5410e3f79c08bb4
David Halkin <halkin.david@gmail.com>
2023-01-10 19:33:03 +0200

    update (header, js, home page and right sidebar

--- app/js/main.js
+++ app/js/main.js
@@ -16,6 +16,26 @@ if ($(".menu-wrapper").length) {
 $(function () {
 	jcf.replaceAll();
 });
-// $(document).ready(function () {
 
-// });
+
+let oCount = document.querySelector("div[countdown]");
+if ($(oCount).length) {
+	oCount.innerText = oCount.countValue = +oCount.getAttribute("countdown");
+	oCount.countRatio = 360 / oCount.countValue;
+	oCount.countColor = 100 / oCount.countValue;
+	oCount.countLight = oCount.countValue / 20;
+	oCount.countTimer = setInterval(fCountdown.bind(oCount), 1000);
+
+	function fCountdown() {
+		let nCount = this.countValue;
+		if (nCount > 0) {
+			nCount--;
+			this.innerText = this.countValue = nCount;
+			this.style.setProperty("--deg", 361 - nCount * this.countRatio);
+			// this.style.setProperty("--col", `hsla(${nCount * this.countColor}, 100%, ${50 - nCount / this.countLight}%, 1)`);
+		} else {
+			clearInterval(this.countTimer);
+			// console.log("Complete " + this.getAttribute('countdown'));
+		}
+	}
+}
